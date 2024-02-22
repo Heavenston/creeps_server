93faecc134ea468bc574e0016ec12774e4003ebb
Malo Legendre-Lemaire <malo.legendre-lemaire@epita.fr>
2024-02-22 04:11:45 +0100

    changed generation

--- game_server/server/terrain/generator.go
+++ game_server/server/terrain/generator.go
@@ -8,13 +8,12 @@ import (
 )
 
 type patch struct {
-	scale  float64
 	thresh float64
 
 	kind             TileKind
 	defaultTileValue uint8
 
-	noise simplex.Noise
+	sample func(x float64, y float64) float64
 }
 
 type ChunkGenerator struct {
@@ -25,7 +24,7 @@ type ChunkGenerator struct {
 
 // First argument is a scale that should be applied to the simplex noise
 // second is the minimum value the noise must be for the tile to be applied
-// (note: simplex noise goes from -1 to 1)
+// (note: simplex noise goes from 0 to 1)
 // so higher threshold means the tile is rarer, and the lower the scale the more
 // 'blury' and big the patches will be
 func (gen *ChunkGenerator) newPath(
@@ -34,14 +33,18 @@ func (gen *ChunkGenerator) newPath(
 	kind TileKind,
 	defaultTileValue uint8,
 ) {
+	noise := simplex.NewNormalized(gen.rand.Int63())
+	
 	var elm = patch {
-		scale:  scale,
 		thresh: thresh,
 
 		kind:             kind,
 		defaultTileValue: defaultTileValue,
 
-		noise: simplex.New(gen.rand.Int63()),
+		sample: func(x, y float64) float64 {
+			v1 := noise.Eval2(x*scale, y*scale)
+			return v1
+		},
 	}
 	gen.patchs = append(gen.patchs, elm)
 }
@@ -52,18 +55,18 @@ func NewChunkGenerator(seed int64) *ChunkGenerator {
 	g.patchs = make([]patch, 0, 0)
 
 	// see newPath docs
-	g.newPath(1./6., 0.6, TileWater, 0)
-	g.newPath(1./5., 0.6, TileStone, 10)
-	g.newPath(1./1., 0.8, TileOil, 10)
-	g.newPath(1./3., 0.5, TileTree, 10)
-	g.newPath(1./3., 0.4, TileBush, 10)
+	g.newPath(1./6., 0.85, TileWater, 0)
+	g.newPath(1./5., 0.85, TileStone, 10)
+	g.newPath(1./1., 0.95, TileOil, 10)
+	g.newPath(1./3., 0.80, TileTree, 10)
+	g.newPath(1./3., 0.75, TileBush, 10)
 
 	return g
 }
 
 func (gen *ChunkGenerator) sample(x int, y int) Tile {
 	for _, patch := range gen.patchs {
-		val := patch.noise.Eval2(float64(x)*patch.scale, float64(y)*patch.scale)
+		val := patch.sample(float64(x), float64(y))
 		if val > patch.thresh {
 			return Tile{
 				Kind:  patch.kind,
