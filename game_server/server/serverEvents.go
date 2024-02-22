b2709019ad635f3247bfe476294827bf01660c39
Malo Legendre-Lemaire <malo.legendre-lemaire@epita.fr>
2024-02-22 11:09:39 +0100

    attempt to fix missing movements

--- game_server/server/serverEvents.go
+++ game_server/server/serverEvents.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	mathutils "creeps.heav.fr/math_utils"
 	"creeps.heav.fr/events"
 	. "creeps.heav.fr/geom"
 	"creeps.heav.fr/spatialmap"
@@ -56,9 +57,19 @@ func (event *UnitMovedEvent) GetAABB() AABB {
 	//        (aabb for From and one for To) but as the current design doesn't
 	//        support finer control I'll stick with this as most movements
 	//        are across adjacent tiles anyways
+
+	min := Point {
+		X: mathutils.Min(event.From.X, event.To.X),
+		Y: mathutils.Min(event.From.Y, event.To.Y),
+	}
+	max := Point {
+		X: mathutils.Max(event.From.X, event.To.X),
+		Y: mathutils.Max(event.From.Y, event.To.Y),
+	}
+
 	return AABB{
-		From: event.From,
-		Size: event.To.Sub(event.From),
+		From: min,
+		Size: max.Sub(min),
 	}
 }
 
