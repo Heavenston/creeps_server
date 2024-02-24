4b345564b3265daa45159c667de044a2f174d246
Malo Legendre-Lemaire <malo.legendre-lemaire@epita.fr>
2024-02-24 02:33:53 +0100

    p shortcut to toggle player usernames

--- front/src/viewer/overlayRenderer.ts
+++ front/src/viewer/overlayRenderer.ts
@@ -8,6 +8,8 @@ export class OverlayRenderer implements IRenderer {
   private eventAbort = new AbortController();
   private players = new Map<string, api.PlayerSpawnMessage>();
 
+  private renderPlayerUsernames = true;
+
   public cleanup() {
     this.eventAbort.abort();
   }
@@ -15,11 +17,19 @@ export class OverlayRenderer implements IRenderer {
   public constructor(renderer: Renderer) {
     this.renderer = renderer;
 
+    document.body.addEventListener("keypress", event => {
+      if (event.key == "p") {
+        this.renderPlayerUsernames = !this.renderPlayerUsernames;
+      }
+    })
+
     api.addEventListener("message", event => {
       const message = event.message;
       if (message.kind != "playerSpawn")
         return;
       this.players.set(message.content.id, message);
+    }, {
+      signal: this.eventAbort.signal,
     });
 
     api.addEventListener("message", event => {
@@ -27,6 +37,8 @@ export class OverlayRenderer implements IRenderer {
       if (message.kind != "playerDespawn")
         return;
       this.players.delete(message.content.id);
+    }, {
+      signal: this.eventAbort.signal,
     });
   }
 
@@ -39,17 +51,21 @@ export class OverlayRenderer implements IRenderer {
 
     const ctx = this.renderer.ctx;
 
+    if (!this.renderPlayerUsernames) {
+      return;
+    }
+
     for (const player of this.players.values()) {
       const sp = vec(player.content.spawnPosition).plus(0.5);
 
       ctx.textAlign = "center";
       ctx.textBaseline = "middle";
 
-      ctx.strokeStyle = "rgba(0, 0, 0, 0.5)";
+      ctx.strokeStyle = "rgba(0, 0, 0, 0.75)";
       ctx.lineWidth = 3 / this.renderer.cameraScale;
       ctx.strokeText(player.content.username, sp.x, sp.y);
       ctx.font = `${18 / this.renderer.cameraScale}px arial`;
-      ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
+      ctx.fillStyle = "rgba(255, 255, 255, 0.75)";
       ctx.fillText(player.content.username, sp.x, sp.y);
     }
   }
